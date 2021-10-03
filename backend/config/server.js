6124fdb06f75c0c06be538beda1e22eeae33bb93
Yousefbrh <a.mozhdi@gmail.com>
2021-10-03 17:14:20 +0330

    backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd06626ffcf3d6f4f53f4042266809201'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4180ab37-2a08-49ee-afa8-6769007d59a8'
+};
